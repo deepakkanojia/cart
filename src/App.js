a1b418fd0b6443fbb487efb887649f616f468f13
Deepak kanojia <kanojiadeepak401@gmail.com>
2020-05-10 21:46:08 +0530

    navbar implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import CartItem from './CartItem';
-import Cart from './Cart'
+import Cart from './Cart';
+import Navbar from './Navbar';
+
 function App() {
   return (
     <div className="App">
-     <h1>Cart</h1>
-     <Cart />
+      <Navbar />
+      <Cart />
     </div>
   );
 }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-
 class CartItem extends React.Component {
   render () {
     console.log('this.props', this.props);
     const { price, title, qty } = this.props.product;
-    const { product , onDecreaseQuantity , onIncreaseQuantity, onDeleteProduct} = this.props;
+    const {
+      product,
+      onIncreaseQuantity,
+      onDecreaseQuantity,
+      onDeleteProduct
+    } = this.props;
     return (
       <div className="cart-item">
+        {this.props.jsx}
         <div className="left-block">
           <img style={styles.image} />
         </div>
@@ -21,20 +26,19 @@ class CartItem extends React.Component {
               alt="increase"
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/992/992651.svg"
-              onClick={()=>this.props.onIncreaseQuantity(product)}
+              onClick={() => onIncreaseQuantity(product)}
             />
             <img
               alt="decrease"
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
-              onClick={()=>this.props.onDecreaseQuantity(product)}
+              onClick={() => onDecreaseQuantity(product)}
             />
             <img
               alt="delete"
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
-              onClick={()=>this.props.onDeleteProduct(product.id)}
-
+              onClick={() => onDeleteProduct(product.id)}
             />
           </div>
         </div>
