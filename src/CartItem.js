df3badd745dbd57204980ac2912b66b6ba396e35
Deepak kanojia <kanojiadeepak401@gmail.com>
2020-05-10 16:54:17 +0530

    cart added

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,80 +1,113 @@
 import React from 'react';
-import App from './App';
 
-class CartItem extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            price : 999,
-            title : 'Mobile Phone',
-            qty : 1,
-            img : ''
+
+class CartItem extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Car',
+      qty: 1,
+      img: '',
         }
-    }
-    increaseQty=()=>{
-        console.log('test', this.state);
-        //set state form 1
-        // this.setState({
-        //     qty : this.state.qty + 1
-        // });
-        //if prev state require
-        this.setState((prevState)=>{
-            return{
-                qty : prevState.qty + 1
-            }
+  
+      
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.testing();
+  }
 
-        })
-    }
-    decreaseQty=()=>{
-        console.log('this', this.state);
-        if(this.state.qty == 0)
-        return;
-        this.setState({
-            qty : this.state.qty - 1
-        });
-    }
-    // deleteQty=()=>{
-    //     if(t)
-    // }
-    render(){
-        const {price ,qty , title} = this.state;
-        return (
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style={style.image} />
-                </div>
-                <div className="right-block">
-                <div style={{fontSize: 25}}>{title}</div>
-                <div style={{color: '#777'}}>Rs {price}</div>
-                <div style={{color : '#777'}}>Qty :{qty}</div>
-                    <div className="cart-item-actions">
-                        {/* {Buttons} */}
-                        <img alt="increase" 
-                        className="action-icon" 
-                        src="https://as2.ftcdn.net/jpg/02/31/96/13/500_F_231961389_VeMu1IMFHWeFiYPfaK6bmT4O8XwxfQC1.jpg" 
-                        height="50" 
-                        onClick = {this.increaseQty}></img>
-                        <img alt="decrease" 
-                        className="action-icon" 
-                        src="https://as2.ftcdn.net/jpg/03/22/51/47/500_F_322514738_MLOhywnD3IgcuataRptf50qq3eR5N5vZ.jpg" 
-                        height="50" onClick = {this.decreaseQty}></img>
-                        <img alt="delete" 
-                        className="action-icon" 
-                        src="https://image.flaticon.com/icons/svg/443/443124.svg" 
-                        height="50" ></img>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-} 
+  // testing () {
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve('done');
+  //     }, 5000);
+  //   })
 
-const style = {
-    image:{
-        height : 110 ,
-        width : 110,
-        borderRadius : 4,
-        background : '#ccc'
+  //   promise.then(() => {
+  //     // setState acts like a synchronus call
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     console.log('state', this.state);
+  //   });
+  // }
+  increaseQuantity = () => {
+    // this.state.qty += 1;
+    // console.log('this', this.state);
+    // setState form 1
+    // this.setState({
+    //   qty: this.state.qty + 1
+    // }, () => {});
+
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+    });
+  }
+
+  decreaseQuantity = () => {
+    const { qty } = this.state;
+
+    if (qty === 0) {
+      return;
     }
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1
+      }
+    });
+  }
+  render () {
+    console.log('render');
+    const { price, title, qty } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/992/992651.svg"
+              onClick={this.increaseQuantity}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+              onClick={this.decreaseQuantity}
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+const styles = {
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: '#ccc'
+  }
+}
+
 export default CartItem;
