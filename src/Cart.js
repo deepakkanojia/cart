2580d57140cd0603ecd5188dbee99046cf2fb53c
Deepak kanojia <kanojiadeepak401@gmail.com>
2020-05-10 17:30:35 +0530

    props

--- src/Cart.js
+++ src/Cart.js
@@ -1,16 +1,50 @@
-
-
 import React from 'react';
 import CartItem from './CartItem';
 
 
 class Cart extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            products : [
+                {
+                          price: 999,
+                          title: 'Car',
+                          qty: 1,
+                          img: '',
+                          id : 1
+                },
+                {
+                        price: 599,
+                          title: 'watch',
+                          qty: 1,
+                          img: '',
+                          id : 2
+                },
+                {
+                            price: 688,
+                          title: 'mobile phone',
+                          qty: 1,
+                          img: '',
+                          id : 3
+                }
+            ]
+            }
+      
+          
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+        // this.testing();
+      }
     render(){
+        const {products} = this.state;
         return (
                 <div className="cart">
-                    <CartItem/>
-                    <CartItem/>
-                    <CartItem/>
+                    {
+                        products.map((product)=>{
+                            return <CartItem product={product} key={product.id}/>
+
+                        })
+                    }
                 </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,20 +2,6 @@ import React from 'react';
 
 
 class CartItem extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      price: 999,
-      title: 'Car',
-      qty: 1,
-      img: '',
-        }
-  
-      
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-    // this.testing();
-  }
-
   // testing () {
   //   const promise = new Promise((resolve, reject) => {
   //     setTimeout(() => {
@@ -64,8 +50,8 @@ class CartItem extends React.Component {
     });
   }
   render () {
-    console.log('render');
-    const { price, title, qty } = this.state;
+    console.log('this.props', this.props);
+    const { price, title, qty } = this.props.product;
     return (
       <div className="cart-item">
         <div className="left-block">
