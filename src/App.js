9882856ffa189109fe15014c375314522068467a
Deepak kanojia <kanojiadeepak401@gmail.com>
2020-05-14 00:13:17 +0530

    added reading data updating data

--- src/App.js
+++ src/App.js
@@ -1,48 +1,95 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
-
+import * as firebase from 'firebase';
 class App extends React.Component {
 
   constructor () {
     super();
     this.state = {
-      products: [
-        {
-          price: 999,
-          title: 'iphone',
-          qty: 1,
-          img: 'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          id: 1
-        },
-        {
-          price: 999,
-          title: 'Watch',
-          qty: 10,
-          img: 'https://images.unsplash.com/photo-1565250026886-ec2964652258?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          id: 2
-        },
-        {
-          price: 999,
-          title: 'Laptop',
-          qty: 4,
-          img: 'https://images.unsplash.com/photo-1479920252409-6e3d8e8d4866?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          id: 3
-        }
-      ]
+      products: [],
+      loading : true
     }
+    this.db = firebase.firestore();
     // this.increaseQuantity = this.increaseQuantity.bind(this);
     // this.testing();
   }
+// mount kerte hai yaha app ko
+  componentDidMount(){
+    //abhi yaha per data store hai firestore me to
+//     firebase
+//     .firestore()
+//     .collection('collection')
+//     //get()will give you snapshot
+//     .get()
+//     //then is func 
+//     .then((snapshot)=>{
+//       console.log(snapshot);
+// // docs.data we use bcz hame data jo collection me hai vo chahiye
+//       snapshot.docs.map((docs)=>{
+//         console.log(docs.data());
+//       })
+// // displaying all data which is present
+//         const products = snapshot.docs.map((doc)=>{
+//           const data = doc.data();
+//           data['id'] = doc.id;
+//           return data;
+//         })
+//         // pahele state use hua ab set state use karenge kyuki data rerender hua
+//         this.setState({
+//           products,
+//           loading : false
+//         })
+//     })
+
+   this.db
+    .collection('collection')
+    //get()will give you snapshot
+    .onSnapshot((snapshot) => {
+      console.log(snapshot)
+
+      snapshot.docs.map((docs)=>{
+                console.log(docs.data());
+              })
+        // displaying all data which is present
+                const products = snapshot.docs.map((doc)=>{
+                  const data = doc.data();
+                  data['id'] = doc.id;
+                  return data;
+                })
+
+                this.setState({
+                            products,
+                            loading : false
+                          })
+    })
+    //then is func 
+  
+  
+  }
+//increase kerne ko sab quantity
   handleIncreaseQuantity = (product) => {
     console.log('Heyy please inc the qty of ', product);
     const { products } = this.state;
+    //taking index of that product
     const index = products.indexOf(product);
+    //abhi product us index per gaya aur quantity increament by 1 
+    // products[index].qty += 1;
 
-    products[index].qty += 1;
+    // this.setState({
+    //   products
+    // })
 
-    this.setState({
-      products
+    const docRef = this.db.collection('collection').doc(products[index].id);
+    docRef
+    .update({
+      qty : products[index].qty + 1
+    })
+    .then(() =>{
+      console.log("updated successfully");
+    })
+    .catch((err)=>{
+      console.log('error in updating' , err);
     })
   }
   handleDecreaseQuantity = (product) => {
@@ -82,17 +129,35 @@ class App extends React.Component {
     return count;
   }
 
+  addProduct = () => {
+   this.db
+    .collection('collection')
+    .add({
+      price : 123,
+      title : "washing machine",
+      img : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5kT-czuJZnBe1AHByJ9EPnMvlECWmQJjSUP0gk7JuxJbgjPd-Du3vf15Ci8wqVlHEOOx0FKCs&usqp=CAc',
+      qty : 12
+    })
+    //ye na sab document ho jayega to document ko console per display karenge
+    .then((docRef) => {
+      console.log('product is added' , docRef);
+    }).catch((err) => {
+      console.log('error is ' , err)
+    })
+  }
   render () {
-    const { products } = this.state;
+    const { products, loading } = this.state;
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
+        {/* <button onClick={this.addProduct} style={{padding : 20 , fontSize: 20}}>  Add a product </button> */}
         <Cart
           products={products}
           onIncreaseQuantity={this.handleIncreaseQuantity}
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
+        {loading && <h1>Loading products...</h1>}
       </div>
     );
   }
